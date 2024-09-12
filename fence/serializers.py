7137a1cc5547aca377e32737acfb6f8530854abd
Admin <matin.mshah@gmail.com>
2024-09-12 23:17:39 +0330

    media

--- fence/serializers.py
+++ fence/serializers.py
@@ -2,12 +2,19 @@ from rest_framework import serializers
 from .models import Product, Category, ProductImage
 
 
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ['id', 'product', 'image']
+
+
 class ProductSerializer(serializers.ModelSerializer):
     formatted_price = serializers.SerializerMethodField()
+    images = ProductImageSerializer(many=True, read_only=True)
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'image', 'category', 'available', 'formatted_price']
+        fields = ['id', 'name', 'description', 'price', 'image', 'category', 'available', 'formatted_price', 'images']
 
     def get_formatted_price(self, obj):
         return obj.formatted_price()
@@ -17,9 +24,3 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'description']
-
-
-class ProductImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProductImage
-        fields = ['id', 'product', 'image']
--- shop/settings.py
+++ shop/settings.py
@@ -140,8 +140,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    # 'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
