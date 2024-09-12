01f281b53607588e626f1aae330474de8775f8b4
Admin <matin.mshah@gmail.com>
2024-09-12 04:22:06 +0330

    fixing allowed host

--- shop/settings.py
+++ shop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-@oh^-!c&k#r0sqirq0)-9hx!26qspwz-ku$cu5czw$6=726^(4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
