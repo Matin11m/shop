beab101bc0af46945dbb741584def4b10d5b512a
Admin <matin.mshah@gmail.com>
2024-09-24 13:27:46 +0330

    delete-commend-code

--- fence/urls.py
+++ fence/urls.py
@@ -8,7 +8,6 @@
 #     path('category/<int:category_id>/', views.category_products, name='category_products'),
 # ]
 
-# urls.py
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
@@ -17,7 +16,7 @@ from .views import ProductViewSet, CategoryViewSet, ProductPaginationViewSet
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
 router.register(r'categories', CategoryViewSet, basename='category')
-router.register(r'productpagination', ProductPaginationViewSet, basename='productpagination')
+router.register(r'product-pagination', ProductPaginationViewSet, basename='product-pagination')
 
 urlpatterns = [
     path('api/', include(router.urls)),
